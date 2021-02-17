e4cc9e38698ff24c36ab583ad87d6555e9e36a09
katekhnguyen <katekhnguyen@gmail.com>
2021-02-16 21:15:06 -0800

    update submit button

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -76,3 +76,50 @@ function progressRing() {
         stop.style.display = "block";
     }
 }
+
+/*** Form Status Submission Animation ***/
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+    var form = document.getElementById("register");
+    var button = document.getElementById("register-btn");
+    var status = document.getElementById("register-btn");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      button.classList.add("submit");
+      status.classList.add("success");
+      status.innerHTML = "Submitted!";
+    }
+  
+    function error() {
+      // status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  
+    // helper function for sending an AJAX request
+  
+    function ajax(method, url, data, success, error) {
+      var xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+          success(xhr.response, xhr.responseType);
+        } else {
+          error(xhr.status, xhr.response, xhr.responseType);
+        }
+      };
+      xhr.send(data);
+    }
+  });
