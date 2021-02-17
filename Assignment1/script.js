44b7e551976ec1323ea5d4a43776cbc4ae12cf90
Nick Marshall-Eminger <hunter3787@gmail.com>
2021-02-16 23:07:14 -0800

    Merge From Sirage

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -46,7 +46,6 @@ function displayImage() {
 }
 
 /*** Form Status Submission Animation ***/
-
 window.addEventListener("DOMContentLoaded", function () {
   // get the form elements defined in your form HTML above
   var form = document.getElementById("register");
@@ -54,7 +53,6 @@ window.addEventListener("DOMContentLoaded", function () {
   var status = document.getElementById("register-btn");
 
   // Success and Error functions for after the form is submitted
-
   function success() {
     form.reset();
     button.classList.add("submit");
@@ -75,7 +73,6 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 
   // helper function for sending an AJAX request
-
   function ajax(method, url, data, success, error) {
     var xhr = new XMLHttpRequest();
     xhr.open(method, url);
@@ -90,4 +87,33 @@ window.addEventListener("DOMContentLoaded", function () {
     };
     xhr.send(data);
   }
+<<<<<<< Updated upstream
+=======
+});
+
+function populateState (){
+  document.getElementById("stateMenu").h  
+
+}
+
+function populateCountry (){
+
+}
+
+const statesEnum = Object.freeze({
+  "Alabama":1,"Alaska":2, "Arizona":3, "Arkansas":4, "California":5,
+  "Colorado":6, "Connecticut":7, "Delaware":8, "Florida":9, "Georgia":10,
+  "Hawaii":11, "Idaho":12, "Illinois":13, "Indiana":14, "Iowa":15, "Kansas":16, 
+  "Kentucky":17, "Lousiana":18, "Maine":19, "Maryland":20, "Massachusetts":21, "Michigan":22,
+  "Minnisota":23, "Mississippi":24, "Missouri":25, "Montana":26, "Nebraska":27, "Nevada":28,
+  "New Hampshire":29, "New Jersey":30, "New Mexico":31, "New York":32, "North Carolina":33, 
+  "North Dakota":34, "Ohio":35, "Oklahoma":36, "Oregon":37, "Pennsylvania":38, "Rhode Island":39,
+  "South Carolina":40, "South Dakota":41, "Tennessee":42, "Texas":43, "Utah":44, "Vermont":45,
+  "Virginia":46, "Washington":47, "West Virginia":48, "Wisconsin":49, "Wyonming":50;
+});
+
+const countryEnum = Object.freeze({
+
+
+>>>>>>> Stashed changes
 });
