27916489fb3885af2b8932cc20bb3aed1f7d996a
katekhnguyen <katekhnguyen@gmail.com>
2021-04-19 00:05:39 -0700

    home page

--- redesign/src/components/DailyDeals.js
+++ redesign/src/components/DailyDeals.js
@@ -1,9 +1,37 @@
 import React from "react";
+import Tick from '@pqina/flip';
+
+console.log(Tick);
+
+function setupFlip(tick) {
+ 
+  Tick.helper.interval(function() {
+
+      tick.value++;
+
+      // Set `aria-label` attribute which screenreaders will read instead of HTML content
+      tick.root.setAttribute('aria-label', tick.value);
+
+  }, 1000);
+}
 
 const DailyDeals = () => {
   return (
     <main>
       <h1>Daily Deals</h1>
+      <div class="clock-counter">
+        <p>
+          These deals are 
+          <br></br>
+          gone forever in
+        </p>
+        <div class= "tick"  data-value="1234" data-did-init="setupFlip">
+          <div data-repeat="true" aria-hidden="true">
+            <span data-view="flip"></span>
+          </div>
+        </div>
+      </div>
+      <h2>List of deals</h2>
     </main>
   );
 };
--- redesign/src/components/Home.js
+++ redesign/src/components/Home.js
@@ -3,7 +3,25 @@ import React from "react";
 const Home = () => {
   return (
     <main>
-      <h1>Home</h1>
+
+      <div class="clock-counter">
+        <p>
+          LATEST DEALS
+          <br></br>
+          DON'T WAIT!
+        </p>
+        <div class= "tick"  data-value="1234" data-did-init="setupFlip">
+          <div data-repeat="true" aria-hidden="true">
+            <span data-view="flip"></span>
+          </div>
+        </div>
+      </div>
+
+      <h1>Trending right now</h1>
+      <p>expanding cards for popular collections</p>
+
+      <h1>Best Selling Shirts</h1>
+
     </main>
   );
 };
