2b512777daf20debec3251fde1d97a7c8d1507ae
Sirage El-Jawhari <60945353+SergeJawhari@users.noreply.github.com>
2021-04-22 03:09:53 -0700

    added footer with responsive social media buttons

---
+++ redesign/src/components/Footer.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter, MDBBtn, MDBIcon } from "mdbreact";
+import {InputGroup, FormControl, Button, Form } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center">
+        <MDBRow>
+          <MDBCol md="6">
+            <h3 className="title">Contact Info</h3>
+            <p>
+                18 Raitherm Road, North York, ON Canada M6B 1S6
+            </p>
+            <li className="list-unstyled">
+                <a href="#!">info@shirtpunch.com</a>
+            </li><br></br>
+
+            <Form.Group controlId="formBasicPassword">
+                <h3 className="title">Join our Newsletter</h3>
+                <Form.Control type="password" placeholder="Email address" />
+                <Button variant="success" type="submit">
+                SUBSCRIBE
+                </Button>
+            </Form.Group>
+
+
+          </MDBCol>
+          <MDBCol md="6">
+            <h5 className="title">Quick Links</h5>
+            <ul>
+              <li className="list-unstyled">
+                <a href="#!">Shipping Policy</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">FAQs</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Refund Policy</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Privacy Policy</a>
+              </li>
+              <li className="list-unstyled">
+                <a href="#!">Terms Of Service</a>
+              </li>
+            </ul>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+      <MDBBtn size="lg" tag="a" floating social="fb">
+        <MDBIcon fab icon="facebook-f" />
+      </MDBBtn>
+      <MDBBtn size="lg" tag="a" floating social="tw">
+        <MDBIcon fab icon="twitter" />
+      </MDBBtn>
+      <MDBBtn size="lg" tag="a" floating social="gplus">
+        <MDBIcon fab icon="google-plus-g" />
+      </MDBBtn>
+      <MDBBtn size="lg" tag="a" floating social="ins">
+        <MDBIcon fab icon="instagram" />
+      </MDBBtn>
+      </div>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.shirtpunch.com/"> Shirtpunch.com </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+  );
+}
+
+export default Footer;
